3bd6ebde2d46ca552a53b5ac9721f68325e31c48
NutanMutakekar <138031482+NutanMutakekar@users.noreply.github.com>
2024-05-01 18:47:56 +0530

    Create 04_star_pyramid.py

---
+++ python/exer_3/04_star_pyramid.py
@@ -0,0 +1,23 @@
+'''Write a Python program to construct the following pattern, using a nested for loop.
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*'''
+
+def print_pattern(rows):
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        print()
+    for i in range(rows - 1, 0, -1):
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        print()
+
+rows = 5
+print_pattern(rows)
